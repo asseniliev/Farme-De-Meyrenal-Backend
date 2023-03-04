2cfae9f782a0b85f7eeacb3b2cdba1f8b9c03899
asseniliev <assen.s.iliev@gmail.com>
2023-03-04 17:27:38 +0100

    transfer vars to .env

--- modules/mailer.js
+++ modules/mailer.js
@@ -5,13 +5,6 @@ async function sendMail(receiverMail, mailTitle, mailText) {
   const { google } = require("googleapis");
   const OAuth2 = google.auth.OAuth2;
 
-  // const EMAIL = "assen1.iliev1@gmail.com";
-  // const REFRESH_TOKEN =
-  //   "1//04kcOV39-gGhTCgYIARAAGAQSNwF-L9IrPtlxgDkDvAccyvf_d3tlpi9bnHA6bBPImf_n4Ve8DvOQ7DRxQmWzc4dfqesjle9unbo";
-  // const CLIENT_SECRET = "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";
-  // const CLIENT_ID =
-  //   "653361233410-cu4g3crv0kb1m85l8mu2l00koumaclef.apps.googleusercontent.com";
-
   const createTransporter = async () => {
     const oauth2Client = new OAuth2(
       process.env.CLIENT_ID,
